a086784031619b3991179c801be9a4402dd6b24d
QuantuMope <43973701+QuantuMope@users.noreply.github.com>
2019-09-17 04:34:04 -0700

    Improve 848 with ternary operator

--- Medium/848_shifting_letters.cpp
+++ Medium/848_shifting_letters.cpp
@@ -8,23 +8,22 @@
 class Solution {
 public:
 
-    void add_wrap_around(char &input, int shift) {
-        /*
-         * Wrap around equation:
-         * a -> 97 , z -> 122
-         * so input = 96 + (input + shift - 122)
-         * =  input = input + shift - 26
-         */
-        if (input + shift <= 122)  input += shift;
-        else                       input += shift - 26;
-    }
+    /*
+     * Wrap around equation:
+     * a -> 97 , z -> 122
+     * so input = 96 + (input + shift - 122)
+     * =  input = input + shift - 26
+     * then
+     * if (input + shift <= 122)  input += shift;
+     * else                       input += shift - 26;
+     */
 
     string shiftingLetters(string S, vector<int>& shifts) {
         int shift {0};
         // Traverse shifts vector in reverse order
         for (int i = shifts.size()-1; i > -1; i--){
             shift = (shift + shifts[i] % 26) % 26;
-            add_wrap_around(S[i], shift);
+            S[i] = (S[i] + shift <= 122) ? S[i] + shift : S[i] + shift - 26;
         }
         return S;
     }
