2b8a5b12ab6ccaa845a4fae8035a0b595d7e06d1
QuantuMope <43973701+QuantuMope@users.noreply.github.com>
2019-09-18 23:50:36 -0700

    Finish 1106

--- Hard/1106_parsing_bool_expr.cpp
+++ Hard/1106_parsing_bool_expr.cpp
@@ -4,127 +4,67 @@
 #include <vector>
 using namespace std;
 
-class Solution1 {
-public:
-
-    string simplifyBool(char op, const int &t_det, const int &f_det) {
-        // ! Case
-        if (op == '!' && t_det > 0) return "f";
-        else if(op == '!') return "t";
-
-        // & Case
-        if (op == '&' && f_det > 0) return "f";
-        else if (op == '&') return "t";
-
-        // | Case
-        if (op == '|' && t_det > 0) return "t";
-        else if (op == '|' && t_det == 0) return "f";
-
-        return "0";
-    }
-
-    string partition(string input) {
-        int t_det {0};
-        int f_det {0};
-
-        if (input.size() <= 1) return input;
-
-        string output {""};
-        int back_paran = 0, front_pos = 0, back_pos = 0;
-        int counter {-1};
-        for (auto &c : input) {
-            counter++;
-            if (c == ')') {
-                back_paran++;
-                back_pos = counter;
-            } else if (c == '(') {
-                front_pos = counter;
-                t_det = f_det = 0;
-            } else if (c == 't') {
-                t_det++;
-            } else if (c == 'f') {
-                f_det++;
-            }
-
-            if (back_paran > 0) break;
-        }
-        front_pos--;
-        output += partition(input.substr(0, front_pos) +
-                            simplifyBool(input[front_pos], t_det, f_det) +
-                            input.substr(back_pos + 1));
-        return output;
-    }
-
-    bool parseBoolExpr(string expression) {
-        string simp = partition(expression);
-        if (simp.find('f') != string::npos) return false;
-        return true;
-    }
-};
-
-
+/**
+ * Time Complexity: On average O(N) where N is length of string
+ *                  Absolute worst case is O(N**2) but only happens in
+ *                  certain string patterns such as continuous hugging
+ *                  parans: e.g. (((((((((((())))))))))))
+ *
+ * Runtime: 12 ms, Faster than 49.26%
+ * Memory: 15.6 MB, (not ranked)
+ */
 class Solution {
 public:
 
     string simplifyBool(string input) {
-         bool t_det {false};
-         bool f_det {false};
-         char op = input[0];
-         for (auto &c : input) {
-             if (t_det && f_det) break;
-             if (c == 't') t_det = true;
-             else if (c == 'f') f_det = true;
-         }
-         if (input.find('t') != string::npos) t_det = true;
-         if (input.find('f') != string::npos) f_det = true;
-         // ! Case
-         if (op == '!' && t_det) return "f";
-         else if(op == '!') return "t";
+        bool t_det {false};
+        bool f_det {false};
+        char op = input[0];
+        string f(input.size(), 'f');
+        string t(input.size(), 't');
+        for (auto &c : input) {
+            if (t_det && f_det) break;
+            if (c == 't') t_det = true;
+            else if (c == 'f') f_det = true;
+        }
+        // ! Case
+        if (op == '!' && t_det) return f;
+        else if(op == '!') return t;
 
-         // & Case
-         if (op == '&' && f_det) return "f";
-         else if (op == '&') return "t";
+        // & Case
+        if (op == '&' && f_det) return f;
+        else if (op == '&') return t;
 
-         // | Case
-         if (op == '|' && t_det) return "t";
-         else if (op == '|') return "f";
+        // | Case
+        if (op == '|' && t_det) return t;
+        else if (op == '|' && !t_det) return f;
 
-         return "0";
+        return "0";
     }
     bool parseBoolExpr(string expression) {
         stack<int> front_paran;
         vector<pair<int, int>> paran_pos;
-        int index {0};
-        while (index < expression.size()) {
-            if (expression[index] == '(') front_paran.push(index);
-            else if (expression[index] == ')') {
-                paran_pos.push_back(pair<int, int>(front_paran.top(), index));
+        for (int i = 0; i < expression.size(); i++) {
+            if (expression[i] == '(') front_paran.push(i);
+            else if (expression[i] == ')') {
+                paran_pos.push_back(pair<int, int>(front_paran.top(), i));
                 front_paran.pop();
             }
-            index++;
         }
-        int counter {1};
         for (auto &paran : paran_pos) {
             int start {paran.first-1};
-            int end {paran.second - paran.first + 2};
+            int end {paran.second - paran.first+1};
             expression.replace(start, end,
                                simplifyBool(expression.substr(start, end)));
-            for (int i = counter; i < paran_pos.size(); i++) {
-                if (paran_pos[i].first < start) paran_pos[i].second -= end -1;
-                else {
-                    paran_pos[i].first -= end - 1;
-                    paran_pos[i].second -= end -1;
-                }
-            }
-            counter++;
         }
 
         return expression.find('t') != string::npos;
     }
 };
 
-int main() {
 
+int main() {
+    // Should be True
     Solution test;
     bool outcome = test.parseBoolExpr("!(&(!(t),&(f),|(f)))");
     cout << boolalpha << outcome << endl;
