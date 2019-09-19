6131cf03accc8e3401f8a9e648b5796f640c68d2
QuantuMope <43973701+QuantuMope@users.noreply.github.com>
2019-09-18 23:05:15 -0700

    Improve 1106 drastically

--- Hard/1106_parsing_bool_expr.cpp
+++ Hard/1106_parsing_bool_expr.cpp
@@ -1,9 +1,10 @@
 #include <string>
 #include <iostream>
-
+#include <stack>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution1 {
 public:
 
     string simplifyBool(char op, const int &t_det, const int &f_det) {
@@ -61,10 +62,62 @@ public:
     }
 };
 
+
+class Solution {
+public:
+
+    string simplifyBool(string input) {
+         int t_det {0};
+         int f_det {0};
+         char op = input[0];
+         string f(input.size(), 'f');
+         string t(input.size(), 't');
+         for (auto &c : input) {
+             if (t_det > 0 && f_det > 0) break;
+             if (c == 't') t_det++;
+             else if (c == 'f') f_det++;
+         }
+         // ! Case
+         if (op == '!' && t_det > 0) return f;
+         else if(op == '!') return t;
+
+         // & Case
+         if (op == '&' && f_det > 0) return f;
+         else if (op == '&') return t;
+
+         // | Case
+         if (op == '|' && t_det > 0) return t;
+         else if (op == '|' && t_det == 0) return f;
+
+         return "0";
+    }
+    bool parseBoolExpr(string expression) {
+        stack<int> front_paran;
+        vector<pair<int, int>> paran_pos;
+        int index {0};
+        while (index < expression.size()) {
+            if (expression[index] == '(') front_paran.push(index);
+            else if (expression[index] == ')') {
+                paran_pos.push_back(pair<int, int>(front_paran.top(), index));
+                front_paran.pop();
+            }
+            index++;
+        }
+        for (auto &paran : paran_pos) {
+            int start {paran.first-1};
+            int end {paran.second - paran.first+1};
+            expression.replace(start, end,
+                               simplifyBool(expression.substr(start, end)));
+        }
+
+        return expression.find('t') != string::npos;
+    }
+};
+
 int main() {
 
     Solution test;
-    bool outcome = test.parseBoolExpr("|(&(t,f,t),!(t))");
+    bool outcome = test.parseBoolExpr("!(&(!(t),&(f),|(f)))");
     cout << boolalpha << outcome << endl;
     return 0;
 }
